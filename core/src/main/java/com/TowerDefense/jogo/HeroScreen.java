80a9cc3f6fac4167d8f7ec4c1e98330ef56586f7
Rfzz1 <138718898+Rfzz1@users.noreply.github.com>
2026-03-21 12:59:22 -0300

    wip: removed panel

--- core/src/main/java/com/TowerDefense/jogo/HeroScreen.java
+++ core/src/main/java/com/TowerDefense/jogo/HeroScreen.java
@@ -467,8 +467,7 @@ public class HeroScreen extends ScreenAdapter {
             } else {
                 batch.draw(heroImagemEstatica, 1050, 580, largura, altura);
             }
-
-            batch.draw(HUDimg[1], 0, 0, 500, 950);
+            
             //Label
         if (labelAtual != null) {
             batch.draw(labelAtual, 700, 950, 700, 120);
